3200d59e2bfe127c000fd2c7a46ce47de5520382
Karmelos <kamil.cyrek06@gmail.com>
2024-02-22 14:53:51 +0100

    test punktow

--- cypress/e2e/logowanie.cy.js
+++ cypress/e2e/logowanie.cy.js
@@ -23,5 +23,30 @@ describe('Test logowania i sprawdzania wartości w localStorage', () => {
       // Jeśli plik istnieje, zaloguj to do konsoli
       cy.log('Plik screen.js został znaleziony na stronie.')
     })
+    // Otwórz stronę z aplikacją
+    cy.visit('http://localhost:3000/screen.html')
+
+    // Pobierz aktualną liczbę punktów z localStorage
+    cy.window().its('localStorage.punkty').then(punkty => {
+      const initialPoints = parseInt(punkty || 0)
+
+      // Kliknij na odpowiedź, która nie zmienia tła na zielone
+      cy.get('#0').click()
+
+      // Poczekaj na zmianę punktów w localStorage
+      cy.wait(2000)
+
+      // Sprawdź, czy punkty się nie zliczają
+      cy.window().its('localStorage.punkty').should('eq', initialPoints.toString())
+
+      // Kliknij na odpowiedź, która zmienia tło na zielone
+      cy.get('#1').click()
+
+      // Poczekaj na zmianę punktów w localStorage
+      cy.wait(2000)
+
+      // Sprawdź, czy punkty się zliczają
+      cy.window().its('localStorage.punkty').should('eq', (initialPoints + 1).toString())
+    })
   })
 })
